065cf6dc610c64b59948118ac144c5d4008ac6aa
mirsking <mirsking@gmail.com>
2015-06-20 15:08:25 +0800

    easy 53 maximum subarray, dp: dp[x+1] = dp[x] + nums[x], dp[x] means the max sum before and include nums[x]

--- easy53_maximum_subarray/easy53_maximum_subarray.cpp
+++ easy53_maximum_subarray/easy53_maximum_subarray.cpp
@@ -5,35 +5,32 @@ using namespace std;
 class Solution {
 public:
     int maxSubArray(vector<int>& nums) {
-		if (nums.size() == 0)
-			return 0;
+        vector<int> dp;
+        dp.resize(nums.size());
+        int max = -INT_MAX;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            if (i == 0)
+                dp[i] = nums[0];
+            else
+            {
+                if (dp[i - 1]>0)
+                    dp[i] = dp[i - 1] + nums[i];
+                else
+                    dp[i] = nums[i];
+            }
 
-		vector< vector<int> > dp(nums.size());
-		int max = -INT_MAX;
-		for (int i = 0; i < nums.size(); i++)
-		{
-			dp[i].resize(nums.size() - i);
-			for (int j = 0; j < nums.size()-i; j++)
-			{
-				if (i == 0 && j == 0)
-					dp[i][j] = nums[i];
+            max = max < dp[i] ? dp[i] : max;
+        }
 
-				if (i!=0)
-					dp[i][j] = dp[i - 1][j+1] - nums[i - 1];
-				else if (j != 0)
-					dp[i][j] = dp[i][i+j-1] + nums[i + j];
-				if (max < dp[i][j])
-					max = dp[i][j];
-			}
-		}
-		return max;
+        return max;
     }
 
 };
 
 int main()
 {
-	int a[] = {-2, 1, -3, 4, -1, 2, 2, -5, 4 };
+	int a[] = {-2, 1, -3, 4, -1, 2, 1, -5, 4 };
 	vector<int> b(a, a + sizeof(a) / sizeof(int));
 	Solution sol;
 	cout << sol.maxSubArray(b);
