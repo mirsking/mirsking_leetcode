d22fecb1ab3e7f1ff12f61e998f3b05d952bec12
Mirs King <mirsking@gmail.com>
2015-06-21 14:57:50 +0800

    easy 203 remove linked list elements

--- easy203_remove_linked_list_elements/easy203_remove_linked_list_elements.cpp
+++ easy203_remove_linked_list_elements/easy203_remove_linked_list_elements.cpp
@@ -12,6 +12,45 @@ struct ListNode {
 class Solution {
 public:
     ListNode* removeElements(ListNode* head, int val) {
-        
+
+		ListNode* head_bak = head;
+		if (head == NULL)
+			return NULL;
+
+		while (head->val == val)
+		{
+			head = head->next;
+			if (head == NULL)
+			{
+				head = head_bak;
+				return NULL;
+			}
+		}
+
+		ListNode* p = new ListNode(head->val);
+		ListNode* res = p;
+		head = head->next;
+
+		while (head!=NULL)
+		{
+			if (head->val != val)
+			{
+				p->next = new ListNode(head->val);
+				p = p->next;
+			}
+			head = head->next;
+		}
+
+		head = head_bak;
+		return res;
     }
 };
+
+int main()
+{
+	ListNode a(1), b(2), c(3);
+	//a.next = &b; b.next = &c;
+	Solution sol;
+	ListNode* re = sol.removeElements(&a, 1);
+	return 0;
+}
