126ba4cd7035b6e53a49d44dfb5292fc85e82f4d
Mirs King <mirsking@gmail.com>
2015-06-28 19:04:20 +0800

    medium 153 find min in rotated sorted array, is there log(n) algorithm?

--- medium153_find_min_in_rotated_sorted_array/medium153_find_min_in_rotated_sorted_array.cpp
+++ medium153_find_min_in_rotated_sorted_array/medium153_find_min_in_rotated_sorted_array.cpp
@@ -5,25 +5,24 @@ using namespace std;
 class Solution {
 public:
     int findMin(vector<int>& nums) {
-		int L = 0;
-		int R = nums.size();
-		while (L < R)
+		if (nums.size() == 0)
+			return INT_MIN;
+		int res = nums[0];
+		for (int i = 1; i < nums.size(); i++)
 		{
-			int M = (L + R) / 2;
-			if (R-L==2 || R-L==1)
-				return min(nums[L], nums[M]);
-
-			if (nums[L] > nums[M])
-				R = M+1;
-			else if (nums[L] < nums[M])
-				L = M;
+			if (res > nums[i])
+			{
+				res = nums[i];
+				break;
+			}
 		}
+		return res;
     }
 };
 
 int main()
 {
-	int a[] = {1,2,3};
+	int a[] = {1,2,1};
 	vector<int> b(a, a + sizeof(a) / sizeof(int));
 	Solution sol;
 	cout << sol.findMin(b);
