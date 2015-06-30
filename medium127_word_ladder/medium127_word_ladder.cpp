f5c48523fb3e7101cf7684676eac782051800ff0
mirsking <mirsking@gmail.com>
2015-06-30 12:24:07 +0800

    medium 127 word ladder, not ok

---
+++ medium127_word_ladder/medium127_word_ladder.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+#include <string>
+#include <unordered_set>
+#include <queue>
+using namespace std;
+class Solution {
+public:
+    int ladderLength(string beginWord, string endWord, unordered_set<string>& wordDict) {
+		queue<string> ladder;
+		ladder.push(beginWord);
+		wordDict.erase(beginWord);
+		int count = 1;
+		int level = 1;
+		while (wordDict.size()>=0 && !ladder.empty())
+		{
+			string curWord = ladder.front();
+			ladder.pop();
+			count--;
+			for (int i = 0; i < curWord.size(); i++)
+			{
+				string tmp = curWord;
+				for (int j = 'a'; j <= 'z'; j++)
+				{
+					if (tmp[i] == j) continue;
+					tmp[i] = j;
+					if (tmp == endWord)
+						return level+1;
+					if (wordDict.find(tmp) != wordDict.end())
+						ladder.push(tmp);
+					wordDict.erase(tmp);
+				}
+			}
+			if (count == 0)
+			{
+				count = ladder.size();
+				level++;
+			}
+
+		}
+		return 0;
+    }
+};
+
+int main()
+{
+	//const int N = 5;
+	//string a[N] = {"hot","dot","dog","lot","log"};
+	const int N = 2;
+	string a[N] = { "hot", "dog" };
+	unordered_set<string> dict;
+	for (int i = 0; i < N; i++)
+	{
+		dict.insert(a[i]);
+	}
+	Solution sol;
+	cout << sol.ladderLength("hot", "dog", dict);
+	return 0;
+}
