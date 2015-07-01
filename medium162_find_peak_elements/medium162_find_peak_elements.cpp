21ee60f4b4a3b67928199566a1e2784210d17d33
mirsking <mirsking@gmail.com>
2015-07-01 11:45:03 +0800

    medium 162 find peak elements, not ok

--- medium162_find_peak_elements/medium162_find_peak_elements.cpp
+++ medium162_find_peak_elements/medium162_find_peak_elements.cpp
@@ -4,18 +4,43 @@ using namespace std;
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-		return findPeakElement(nums.begin(), nums.end());
+		nums.insert(nums.begin(), INT_MIN);
+		nums.insert(nums.end(), INT_MIN);
+		return findPeakElement(nums.begin() + 1, nums.end() - 1, nums.end()) - nums.begin();
     }
 private:
 	template<typename BidiIt>
-	int findPeakElement(BidiIt begin, BidiIt end)
+	BidiIt findPeakElement(BidiIt begin, BidiIt end, const BidiIt flag)
 	{
-		BidiIt mid = (begin + end) / 2;
-		int lres = findPeakElement(begin, mid);
-		int rres = findPeakElement(mid, end);
-		if (lres >= 0)
+		BidiIt mid = (end - begin) / 2 + begin;
+		if (isPeak(mid))
+			return mid;
+
+		BidiIt lres = findPeakElement(begin, mid, flag);
+		if (lres != flag)
 			return lres;
-		else if (rres > 0)
+
+		BidiIt rres = findPeakElement(mid, end, flag);
+		if (rres != flag)
 			return rres;
+
+		return flag;
+	}
+	template<typename BidiIt>
+	bool isPeak(BidiIt mid)
+	{
+		if (*mid > *next(mid) && *mid > *prev(mid))
+			return true;
+		else
+			return false;
 	}
 };
+
+int main()
+{
+	int a[] = {1,2,3,1};
+	vector<int> b(a, a + sizeof(a) / sizeof(int));
+	Solution sol;
+	cout << sol.findPeakElement(b);
+	return 0;
+}
