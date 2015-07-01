85c85b755fd3199bf5ca755aaea54894a3485c0c
Mirs King <mirsking@gmail.com>
2015-07-01 14:15:04 +0800

    medium 162 find peak elements

--- medium162_find_peak_elements/medium162_find_peak_elements.cpp
+++ medium162_find_peak_elements/medium162_find_peak_elements.cpp
@@ -4,35 +4,50 @@ using namespace std;
 class Solution {
 public:
     int findPeakElement(vector<int>& nums) {
-		nums.insert(nums.begin(), INT_MIN);
-		nums.insert(nums.end(), INT_MIN);
-		return findPeakElement(nums.begin() + 1, nums.end() - 1, nums.end()) - nums.begin();
+		return findPeakElement(nums.begin(), nums.end(),nums.begin(), nums.end()) - nums.begin();
     }
 private:
 	template<typename BidiIt>
-	BidiIt findPeakElement(BidiIt begin, BidiIt end, const BidiIt flag)
+	BidiIt findPeakElement(BidiIt begin, BidiIt end, const BidiIt begin_flag, const BidiIt end_flag)
 	{
+		if (end - begin == 1)
+		{
+			if (isPeak(begin_flag, end_flag, begin))
+				return begin;
+			else
+				return end_flag;
+		}
+
 		BidiIt mid = (end - begin) / 2 + begin;
-		if (isPeak(mid))
+		cout << *begin << " " << *mid << " " << *prev(end) << endl;
+		if (isPeak(begin_flag, end_flag, mid))
 			return mid;
 
-		BidiIt lres = findPeakElement(begin, mid, flag);
-		if (lres != flag)
+		BidiIt lres = findPeakElement(begin, mid, begin_flag, end_flag);
+		BidiIt rres = findPeakElement(mid, end, begin_flag, end_flag);
+		if (lres != end_flag)
 			return lres;
-
-		BidiIt rres = findPeakElement(mid, end, flag);
-		if (rres != flag)
+		if (rres != end_flag)
 			return rres;
 
-		return flag;
+		return end_flag;
 	}
 	template<typename BidiIt>
-	bool isPeak(BidiIt mid)
+	bool isPeak(const BidiIt begin, const BidiIt end, BidiIt mid)
 	{
-		if (*mid > *next(mid) && *mid > *prev(mid))
+		if (mid != begin && mid != prev(end))
+		{
+			if (*mid > *next(mid) && *mid > *prev(mid))
+				return true;
+			else
+				return false;
+		}
+		else if (mid == begin && mid == prev(end))
 			return true;
-		else
-			return false;
+		else if (mid == begin)
+			return *mid > *next(mid);
+		else if (mid == prev(end))
+			return *mid > *prev(mid);
 	}
 };
 
