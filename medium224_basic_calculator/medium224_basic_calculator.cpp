00257b6e95b9067e9f7c8cc15986aaa97765b86c
mirsking <mirsking@gmail.com>
2015-07-07 09:01:30 +0800

    medium 224 basic calculator, post fix expression, how to handle nums and operators simultaneously.

--- medium224_basic_calculator/medium224_basic_calculator.cpp
+++ medium224_basic_calculator/medium224_basic_calculator.cpp
@@ -1,115 +1,102 @@
+#include <stack>
 #include <iostream>
 #include <string>
-#include <stack>
-#include <algorithm>
+#include <vector>
 using namespace std;
+
 class Solution {
 public:
     int calculate(string s) {
-		int index = 0;
-		stack<string> sta;
-		return calculate(s, 0, sta);
+        vector<long> postfix = to_postfix(s);
+        int len = postfix.size();
+        if (len == 0) {
+            return 0;
+        }
+        vector<long> tmp;
+        long a, b;
+        for (int i=0; i<len; i++) {
+            long ch = postfix[i];
+            switch(ch) {
+                case '+':
+                    a = tmp.back();
+                    tmp.pop_back();
+                    tmp.back() = tmp.back() + a;
+                    break;
+                case '-':
+                    a = tmp.back();
+                    tmp.pop_back();
+                    tmp.back() = tmp.back() - a;
+                    break;
+                default:
+                    tmp.push_back(ch);
+            }
+        }
+        return tmp[0];
     }
-
-private:
-	int calculate(string s, int index, stack<string>& sta) {
-		string tmp = "";
-		for (int i = index; i < s.size(); i++)
-		{
-			auto& ch = s[i];
-			if (ch == ' ')
-				continue;
-
-			if (ch != '+' && ch != '-' && ch != '(' && ch != ')')
-			{
-				tmp.push_back(ch);
-			}
-			else if (ch == '+' || ch == '-' || ch=='(')
-			{
-				sta.push(tmp);
-				tmp = "";
-				string tmp1;
-				tmp1.push_back(ch);
-				sta.push(tmp1);
-			}
-			else if (ch == ')')
-			{
-				if (tmp != "")
-				{
-					sta.push(tmp);
-					tmp = "";
-				}
-				auto& ch1 = sta.top();
-				stack<string> sta_tmp;
-				while (ch1 != "(")
-				{
-					sta_tmp.push(sta.top());
-					sta.pop();
-					ch1 = sta.top();
-				}
-				sta.pop();
-				int tmp=0;
-				while (!sta_tmp.empty())
-				{
-					auto& str = sta_tmp.top();
-					if (str != "+" && str!="-")
-					{
-						tmp += stoi(str);
-						sta_tmp.pop();
-					}
-					else if (str == "+")
-					{
-						sta_tmp.pop();
-						tmp += stoi(sta_tmp.top());
-						sta_tmp.pop();
-					}
-					else if (str == "-")
-					{
-						sta_tmp.pop();
-						tmp -= stoi(sta_tmp.top());
-						sta_tmp.pop();
-					}
-				}
-				sta.push(to_string(tmp));
-			}
-		}
-
-		stack<string> sta_tmp;
-		while (!sta.empty())
-		{
-			sta_tmp.push(sta.top());
-			sta.pop();
-		}
-		int res = 0;
-		while (!sta_tmp.empty())
-		{
-			if (sta_tmp.top() == "+")
-			{
-				sta_tmp.pop();
-				res += stoi(sta_tmp.top());
-				sta_tmp.pop();
-			}
-			else if (sta_tmp.top() == "-")
-			{
-				sta_tmp.pop();
-				res -= stoi(sta_tmp.top());
-				sta_tmp.pop();
-			}
-			else
-			{
-				res += stoi(sta_tmp.top());
-				sta_tmp.pop();
-			}
-		}
-		return res;
-	}
+    
+    vector<long> to_postfix(const string& s) {
+        int len = s.size();
+        // operators
+        vector<char> operato;
+        // generated postfix experssion of this infix experssion
+        vector<long> postfix;
+        
+        int val = 0;
+        bool innum = false;
+        
+        for (int i=0; i<len; i++) {
+            char ch = s[i];
+            switch (ch) {
+                case ' ':
+                    // skip space
+                    continue;
+                case '-':
+                case '+':
+                    while (!operato.empty() && operato.back() != '(') {
+                        postfix.push_back(operato.back());
+                        operato.pop_back();
+                    }
+                    operato.push_back(ch);
+                    break;
+                case '(':
+                    // just push to operato
+                    operato.push_back(ch);
+                    break;
+                case ')':
+                    // move any operato between this ')' and it's paired '('
+                    while (!operato.empty() && operato.back() != '(') {
+                        postfix.push_back(operato.back());
+                        operato.pop_back();
+                    }
+                    // destroy '(' placeholder
+                    operato.pop_back();
+                    break;
+                default:
+                    if (innum) {
+                        val = val * 10 + ch - '0';
+                    } else {
+                        val = ch - '0';
+                        innum = true;
+                    }
+                    // look ahead
+                    if (i+1 == len || s[i+1] > '9' || s[i+1] < '0') {
+                        postfix.push_back(val);
+                        innum = false;
+                    }
+            }
+        }
+        
+        while (!operato.empty()) {
+            postfix.push_back(operato.back());
+            operato.pop_back();
+        }
+        return postfix;
+    }
 };
 
 int main()
 {
-	Solution  sol;
-	string a = to_string('+');
-	string b = "+";
-	cout << sol.calculate("(100+(4+5+2)-3)+(6+8)") << endl;
+	Solution sol;
+	cout << sol.calculate("(45+(4+5+2)-3)+(6+8)") << endl;
 	return 0;
 }
