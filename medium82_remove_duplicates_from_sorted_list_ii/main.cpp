a2e9eeaad7d298156bbd199ab51f7545be9a762a
mirsking <mirsking@gmail.com>
2016-02-03 21:35:02 +0800

    (nice to review, deleting not new vars gets error in debug mode)medium 82 remove duplicates from sorted list ii

--- medium82_remove_duplicates_from_sorted_list_ii/main.cpp
+++ medium82_remove_duplicates_from_sorted_list_ii/main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
 #include <unordered_map>
 #include <vector>
+#include <climits>
+#include <unordered_set>
+#include <algorithm>
 using namespace std;
 /**
  * Definition for singly-linked list.
@@ -14,46 +17,39 @@ struct ListNode {
 class Solution {
 public:
     ListNode* deleteDuplicates(ListNode* head) {
-        if(!head)
-            return NULL;
-
-        ListNode *res = NULL;
-        ListNode *cur = head;
-        bool cur_fail = false;
-        while(cur && cur->next)
-        {
-            if(cur->next->val != cur->val)
-            {
-                if(!cur_fail)
-                {
-                    if(!res)
-                        res = cur;
-                    else
-                        res->next = cur;
-                    cur = cur->next;
-                }
-                else // cur_fail == true
-                    cur_fail = false;
-            }
-            else
-            {
-                cur = cur->next;
-                cur_fail = true;
-            }
-        }
-        return res;
+		if (!head || !head->next)
+			return head;
+		ListNode* p = head->next;
+		if (head->val == p->val)
+		{
+			while (p && head->val == p->val)
+			{
+				ListNode* tmp = p;
+				p = p->next;
+				delete tmp;
+			}
+			delete head;
+			return deleteDuplicates(p);
+		}
+		else
+		{
+			head->next = deleteDuplicates(head->next);
+			return head;
+		}
     }
 };
 
 int main()
 {
-    ListNode a(1), b(1), c(2), d(2), e(3);
+	ListNode a(1), b(1), c(2), d(2), e(3), f(4), g(4), h(5);
     a.next = &b; b.next = &c; c.next = &d; d.next = &e;
+	e.next = &f; f.next = &g; g.next = &h;
     Solution sol;
     ListNode* res =sol.deleteDuplicates(&a);
     while(res)
     {
         cout << res->val << " ";
+		res = res->next;
     }
 
     return 0;
