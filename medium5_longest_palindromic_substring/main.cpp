393ab75141dbf92a54659f5b367c23c3ac1e8710
mirsking <mirsking@gmail.com>
2016-02-04 19:35:23 +0800

    (nice to review) medium 5 longest palindromic substring, in VS complier, Memory Limit Exceed.

--- medium5_longest_palindromic_substring/main.cpp
+++ medium5_longest_palindromic_substring/main.cpp
@@ -1,3 +1,5 @@
+#pragma warning(disable:4996)
+//#define _SCL_SECURE_NO_WARNINGS
 #include <iostream>
 #include <string>
 #include <vector>
@@ -6,12 +8,25 @@ using namespace std;
 class Solution {
 public:
 	string longestPalindrome(string s) {
-		int N = s.size();
-		vector<vector<bool> > dp(N, vector<bool>(N, false));
+		const int N = s.size();
+		//vector<vector<bool> > dp(N, vector<bool>(N, false));
+#ifdef WIN32
+		bool **dp;
+		dp = new bool*[N];
+		for(int i=0; i<N; i++)
+		{
+			dp[i] = new bool[N];
+			memset(dp[i], 0, N);
+		}
+#else
+		bool dp[N][N];
+		fill_n(&dp[0][0], N*N, false);
+#endif
+
 		for (int i = 0; i < N; i++)
 			dp[i][i] = true;
 
-		int start = 0; int max_len = 0;
+		int start = 0; int max_len = 1; // notice: min_len is 1 not 0
 		for (int j = 0; j < N; j++)
 		{
 			for (int i = 0; i < j; i++)
@@ -27,6 +42,11 @@ public:
 				}
 			}
 		}
+#ifdef WIN32
+		for (int i = 0; i < N; i++)
+			delete[] dp[i];
+		delete[] dp;
+#endif
 		return s.substr(start, max_len);
 	}
 };
