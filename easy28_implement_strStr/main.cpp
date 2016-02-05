4dcf492051cabf7bcc20210e7c371b3079df4987
mirsking <mirsking@gmail.com>
2016-02-05 13:14:05 +0800

    (very very nice to review) easy 28 implement strStr, KMP algorithm, see http://www.cnblogs.com/c-cloud/p/3224788.html

--- easy28_implement_strStr/main.cpp
+++ easy28_implement_strStr/main.cpp
@@ -5,6 +5,53 @@ using namespace std;
 class Solution {
 public:
 	int strStr(string haystack, string needle) {
+		return kmp(haystack.c_str(), needle.c_str());
+	}
+private:
+	
+	static void computePrefix(const char *pattern, int next[])
+	{
+		int i;
+		int j = -1;
+		const int m = strlen(pattern);
+
+		next[0] = j;
+		for (i = 1; i < m; i++)
+		{
+			while (j > -1 && pattern[j + 1] != pattern[i])
+				j = next[j];
+
+			if (pattern[i] == pattern[j + 1])
+				j++;
+			next[i] = j;
+		}
+	}
+
+	static int kmp(const char *text, const char *pattern)
+	{
+		int i;
+		int j = -1;
+		const int n = strlen(text);
+		const int m = strlen(pattern);
+		if (m == 0) return 0; //for the case pattern = ""
+
+		int *next = new int[m];
+		computePrefix(pattern, next);
+		for (i = 0; i < n; i++) {
+			while (j > -1 && pattern[j + 1] != text[i]) j = next[j];
+			if (text[i] == pattern[j + 1]) j++;
+			if (j == m - 1) {
+				delete next;
+				return i - j;
+			}
+		}
+		delete next;
+		return -1;
+	}
+
+
+	/* Brute Force Algorithm
+	int strStr(string haystack, string needle) {
 		int n = haystack.size();
 		int m = needle.size();
 
@@ -26,6 +73,7 @@ public:
 		}
 		return -1;
 	}
+	*/
 };
 
 int main()
