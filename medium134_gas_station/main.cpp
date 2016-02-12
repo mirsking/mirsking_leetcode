631e4778cfa38a4e4392461ac6b62408332de5de
mirsking <mirsking@gmail.com>
2016-02-12 13:34:13 +0800

    (nice to review)medium 134 gas station, o(n) algorithm, use total to identify the valid of the array.

--- medium134_gas_station/main.cpp
+++ medium134_gas_station/main.cpp
@@ -5,6 +5,23 @@ using namespace std;
 class Solution {
 public:
 	int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
+		int total = 0;
+		int j = -1;
+		int sum = 0;
+		for (int i = 0; i < gas.size(); i++)
+		{
+			sum += gas[i] - cost[i];
+			total += gas[i] - cost[i];
+			if (sum < 0)
+			{
+				j = i;
+				sum = 0;
+			}
+		}
+		return total >= 0 ? j + 1 : -1;
+	}
+	/* TLE
+	int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
 		int N = gas.size();
 		for (int i = 0; i < N; i++)
 		{
@@ -21,6 +38,7 @@ public:
 		}
 		return -1;
 	}
+	*/
 };
 
 int main()
