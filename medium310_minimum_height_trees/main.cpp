f7cad1853c8e9cb635e095d3e9c0d610ad2b38ab
mirsking@gmail.com <mirsking>
2016-02-21 18:54:21 +0800

    (nice to review) medium 310 minimum height trees

--- medium310_minimum_height_trees/main.cpp
+++ medium310_minimum_height_trees/main.cpp
@@ -3,6 +3,60 @@
 #include <map>
 #include <queue>
 using namespace std;
+
+class Solution {
+public:
+	//Õÿ∆À≈≈–Ú
+	vector<int> findMinHeightTrees(int n, vector<pair<int, int>>& edges) {
+		if (n == 1) // for the case "1 []"
+			return { 0 };
+
+		vector<int> degrees(n, 0);
+		queue<int> que;
+		vector<vector<int>> graph(n);
+		for (auto& e : edges)
+		{
+			graph[e.first].push_back(e.second);
+			degrees[e.first]++;
+
+			graph[e.second].push_back(e.first);
+			degrees[e.second]++;
+		}
+		for (int i = 0; i < n; i++)
+		{
+			if (degrees[i] == 1)
+				que.push(i);
+		}
+		while (n>2)
+		{
+			int sz = que.size();
+			if (sz == 0)
+				break;
+			for (int i = 0; i < sz; i++)
+			{
+				int node = que.front();
+				que.pop();
+				n--;
+				for (auto t : graph[node])
+				{
+					degrees[t]--;
+					if (degrees[t] == 1)
+						que.push(t);
+				}
+			}
+		}
+
+		vector<int> res;
+		while (!que.empty())
+		{
+			res.push_back(que.front());
+			que.pop();
+		}
+		return res;
+
+	}
+};
+/*
 class Solution {
 public:
     vector<int> findMinHeightTrees(int n, vector<pair<int, int>>& edges) {
@@ -68,13 +122,16 @@ private:
 		bfs(graph, que2, visited, last_height + 1, height);
 	}
 };
+*/
 
 int main()
 {
 	//int n = 4;
 	//vector<pair<int, int>> input = { { 1, 0 }, { 1, 2 }, { 1, 3 } };
-	int n = 6;
-	vector<pair<int, int>> input = {{0, 3}, {1, 3}, {2, 3}, {4, 3}, {5, 4}};
+	//int n = 6;
+	//vector<pair<int, int>> input = {{0, 3}, {1, 3}, {2, 3}, {4, 3}, {5, 4}};
+	int n = 11;
+	vector<pair<int, int>> input = { { 0, 1 }, { 0, 2 }, { 2, 3 }, { 0, 4 }, { 2, 5 }, { 5, 6 }, { 3, 7 }, { 6, 8 }, { 8, 9 }, { 9, 10 } };
 	Solution sol;
 	auto res = sol.findMinHeightTrees(n, input);
 	for (auto& r : res)
