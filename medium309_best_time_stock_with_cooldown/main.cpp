1dd86a88ebcc071273603e396122a1b55e69b2a2
mirsking@gmail.com <mirsking>
2016-02-21 15:58:02 +0800

    medium 309 best time stock with cooldown

---
+++ medium309_best_time_stock_with_cooldown/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy = INT_MIN, pre_buy = 0, sell = 0, pre_sell = 0;
+        for (int price : prices) {
+            pre_buy = buy;
+            buy = max(pre_sell - price, pre_buy);
+            pre_sell = sell;
+            sell = max(pre_buy + price, pre_sell);
+        }
+        return sell;
+    }
+};
+int main()
+{
+	return 0;
+}
