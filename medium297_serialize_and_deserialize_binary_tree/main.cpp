66d480c9521d74513356bae4b8a1ee1ae64bd73d
mirsking@gmail.com <mirsking>
2016-02-22 10:03:23 +0800

    (very nice to review) use PreOrder Travesal to code and decode tree

--- medium297_serialize_and_deserialize_binary_tree/main.cpp
+++ medium297_serialize_and_deserialize_binary_tree/main.cpp
@@ -13,65 +13,50 @@ struct TreeNode {
     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 class Codec {
+	//http://www.cnblogs.com/grandyang/p/4913869.html
 public:
 
     // Encodes a tree to a single string.
     string serialize(TreeNode* root) {
-		vector<string> res_vec;
-		IndexTravesal(root, 0, res_vec);
-		string res;
-		for (int i = 0; i < res_vec.size(); i++)
-		{
-			if (res_vec[i] != "")
-				res += res_vec[i];
-			else
-				res += "n";
-			if (i + 1 != res_vec.size())
-				res += " ";
-		}
-		return res;
+		stringstream ss;
+		serialize(root, ss);
+		return ss.str();
     }
 
     // Decodes your encoded data to tree.
     TreeNode* deserialize(string data) {
-		vector<string> nodes;
-		str2Vector(data, nodes);
-		return deserialize(nodes, 0);
+		stringstream ss(data);
+		return deserialize(ss);
     }
 
 private:
-	void IndexTravesal(TreeNode* root, int id, vector<string>& res)
+	//preorder
+	void serialize(TreeNode* root, stringstream& ss)
 	{
-		if (!root)
-			return;
-		if (id >= res.size())
-			res.resize(id + 1);
-		res[id] = to_string(root->val);
-		IndexTravesal(root->left, 2*id + 1, res);
-		IndexTravesal(root->right, 2*id + 2, res);
-	}
-	TreeNode* deserialize(vector<string>& nodes, int id)
-	{
-		if (id >= nodes.size() || nodes[id] == "n")
-			return NULL;
-
-		int val = stoi(nodes[id]);
-		TreeNode* root = new TreeNode(val);
-		root->left = deserialize(nodes, id * 2 + 1);
-		root->right = deserialize(nodes, id * 2 + 2);
-		return root;
-	}
-	void str2Vector(string& str, vector<string>& res)
-	{
-		stringstream ss(str);
-		while (ss)
+		if (root)
 		{
-			string tmp;
-			ss >> tmp;
-			if (tmp != "")
-				res.push_back(tmp);
+			ss << root->val << ' ';
+			serialize(root->left, ss);
+			serialize(root->right, ss);
+		}
+		else
+		{
+			ss << "# ";
 		}
 	}
+
+	TreeNode* deserialize(stringstream& ss)
+	{
+		string val;
+		ss >> val;
+		if (val == "#")
+			return NULL;
+		TreeNode *root = new TreeNode(stoi(val));
+		root->left = deserialize(ss);
+		root->right = deserialize(ss);
+		return root;
+	}
+
 };
 
 // Your Codec object will be instantiated and called as such:
@@ -83,7 +68,7 @@ int main()
 	TreeNode a(1), b(2), c(3), d(4), e(5);
 	a.left = &b; a.right = &c; c.left = &d;// c.right = &e;
 	Codec cod;
-	string res = cod.serialize(NULL);
+	string res = cod.serialize(&a);
 	TreeNode* res1 = cod.deserialize(res);
 	return 0;
 }
